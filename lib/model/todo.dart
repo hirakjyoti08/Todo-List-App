644fbf37c5a11341e1d65dfa923670531c8bcaef
Hirakjyoti Talukdar <u20cse1041@cit.ac.in>
2024-01-09 01:28:44 +0530

    changes in todo.dart

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -13,7 +13,7 @@ class ToDo {
     return [
       ToDo(id: '01', todoText: 'Morning Yoga', isDone: true),
       ToDo(id: '02', todoText: 'Revise yesterday Concept', isDone: true),
-      ToDo(id: '03', todoText: 'Gate Lec 1', ),
+      ToDo(id: '03', todoText: 'Gate Lec 1', isDone: true ),
       ToDo(id: '04', todoText: 'Solve PYQ', ),
       ToDo(id: '05', todoText: 'Evening Coding', ),
       ToDo(id: '06', todoText: 'Gate Lec 2', ),
